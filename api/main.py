5e6a1a5d4e44cbe3b3179e1328a6bb1e334c3a43
Ashmit Pandey <pandeyashmit299@gmail.com>
2026-08-29 12:23:58 +0530

    fix: allow all CORS origins to resolve frontend connection issue

--- api/main.py
+++ api/main.py
@@ -25,11 +25,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://vana.blackholeinfiverse.com",
-        "http://localhost:8000",
-        "http://127.0.0.1:8000",
-    ],
+    allow_origins=["*"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
